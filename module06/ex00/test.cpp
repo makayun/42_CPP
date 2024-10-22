f73c5039a2da5cb35869be82b6079f948a141bda
Maksim Makagonov <maxmakagonov@Maksims-MacBook-Pro.local>
2024-10-22 09:54:58 +0200

    module 06

---
+++ module06/ex00/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+bool convert(const std::string& input) {
+	
+}
+
+int main() {
+	std::string	input[4] = {"A", "1", "2.0f", "3.0"};
+
+	for (int i = 0; i < 4; i++)
+		convert(input[i]);
+}
+
+
