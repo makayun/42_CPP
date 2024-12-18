727ee21c06fab0bcd283dfbae2e1e70626ce4dce
Maksim Makagonov <mmakagon@c2r10s3.42prague.com>
2024-12-18 13:12:00 +0100

    learn the merge sort

--- module09/ex02/main.cpp
+++ module09/ex02/main.cpp
@@ -6,14 +6,14 @@
 /*   By: mmakagon <mmakagon@student.42.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 14:44:06 by mmakagon          #+#    #+#             */
-/*   Updated: 2024/12/16 15:38:37 by mmakagon         ###   ########.fr       */
+/*   Updated: 2024/12/18 13:11:29 by mmakagon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <sstream>
-#include <vector>
 #include <list>
+#include <deque>
 
 // ./PmergeMe `shuf -i 1-100000 -n 5 | tr "\n" " "`
 
@@ -26,13 +26,59 @@ Print the result
 Print times
 */
 
+#define PRINT_VAR(x) std::cout << #x << ": " << x << std::endl
+
+std::ostream& operator<<( std::ostream& out, const std::deque<int>& in)
+{
+	for (std::deque<int>::const_iterator it = in.begin(); it != in.end(); ++it)
+		out << *it << " ";
+	return (out);
+}
+
+void pjDequeSort(std::deque<int>& a) {
+	std::deque<int> b;
+	size_t i = 0;
+
+	while (i <= a.size() / 2) {
+		b.push_back(a[i]);
+		a.erase(a.begin() + i);
+		if (a[i] < b[i])
+			std::swap(a[i], b[i]);
+		PRINT_VAR(a);
+		PRINT_VAR(b);
+		++i;
+	}
+
+	i = 1;
+	size_t j;
+	int temp_a;
+	int temp_b;
+	int index;
+	while (i < b.size()) {
+		temp_a = a[i];
+		temp_b = b[i];
+		a.erase(a.begin() + i);
+		b.erase(b.begin() + i);
+		j = i - 1;
+		for ( ; j > 0; --j)
+			if (a[j] >= temp_a)
+				index = j;
+		a.insert(a.begin() + index, temp_a);
+		b.insert(b.begin() + index, temp_b);
+		++i;
+	}
+	PRINT_VAR(a);
+	PRINT_VAR(b);
+
+}
+
 int main(int argc, char** argv) {
 	if (argc < 2) {
 		std::cerr << "Try again" << std::endl;
 		return (42);
 	}
 
-	std::vector<int>	v;
+	std::deque<int>		d;
 	std::list<int>		l;
 
 	{
@@ -46,14 +92,11 @@ int main(int argc, char** argv) {
 				std::cerr << "Error" << std::endl;
 				return (42);
 			}
-			v.push_back(temp);
+			d.push_back(temp);
 			l.push_back(temp);
 		}
 	}
-
-	std::cout << "Before:";
-	for (size_t i = 0; i < v.size(); ++i)
-		std::cout << " " << v[i];
-	std::cout << std::endl;
+	PRINT_VAR(d);
+	pjDequeSort(d);
 }
 
