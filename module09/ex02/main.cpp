63c23913ff2dc7fa4511730ce57e566120506cd9
Maksim Makagonov <maxmakagonov@Maksims-MacBook-Pro.local>
2025-01-09 11:38:18 +0100

    kurvadopici

--- module09/ex02/main.cpp
+++ module09/ex02/main.cpp
@@ -1,14 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mmakagon <mmakagon@student.42.com>         +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/16 14:44:06 by mmakagon          #+#    #+#             */
-/*   Updated: 2025/01/09 08:16:52 by mmakagon         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
@@ -28,8 +17,6 @@ void findMe(const std::deque<int>& d) {
 }
 
 void dequeJacubsthal(std::deque<size_t>& jcbsthl, size_t size) {
-	SECTION("Jacubsthal array generation");
-	PRINT_VAR(size);
 	jcbsthl.clear(); // just in case
 	jcbsthl.push_back(0);
 	size_t	current_jcb = 1;
@@ -43,7 +30,6 @@ void dequeJacubsthal(std::deque<size_t>& jcbsthl, size_t size) {
 		next_jcb += current_jcb * 2;
 		current_jcb = next_jcb - current_jcb * 2;
 	}
-	PRINT_VAR(jcbsthl);
 }
 
 struct SmallerChunks {
@@ -53,16 +39,16 @@ struct SmallerChunks {
 	SmallerChunks(std::deque<int>& d, std::size_t cs) : deq(d), chunk_size(cs) {}
 
 	bool operator()(const int& value) const {
-		std::ptrdiff_t index = &value - &deq[0];
+		size_t i = 0;
+		for ( ;i < deq.size(); ++i)
+			if (&deq[i] == &value)
+				break;
 
-		return (index / chunk_size) % 2 != 0;
+		return (i / chunk_size) % 2 != 0;
 	}
 };
 
 void fjDequeWrap(std::deque<int>& in_arr, size_t chunk_size) {
-	SECTION("\nWrapping iteration");
-	PRINT_VAR(chunk_size);
-
 	std::deque<int>::iterator a;
 	std::deque<int>::iterator b;
 
@@ -79,32 +65,23 @@ void fjDequeWrap(std::deque<int>& in_arr, size_t chunk_size) {
 
 			if (*a > *b)
 				std::swap_ranges(a, a_end, b);
+			++comparisons;
 		}
-		++comparisons;
 	}
-	SECTION("After wrapping:");
-	PRINT_VAR(in_arr);
-	PRINT_VAR(comparisons);
 }
 
 void fjDequeMakeTail(std::deque<int>& a, std::deque<int>& tail, size_t chunk_size) {
 	tail.resize(a.size() % chunk_size);
 	if (tail.size()) {
-		SECTION("Making a tail");
 		std::deque<int>::const_iterator tail_in_a = a.cend();
 		std::advance(tail_in_a, -tail.size());
 		std::copy(tail_in_a, a.cend(), tail.begin());
 		a.resize(a.size() - tail.size());
-		PRINT_VAR(a);
-		PRINT_VAR(tail);
 	}
 }
 
-void fjDequeSmallerChunksToB(std::deque<int>& a, std::deque<int>& b, std::deque<int>& unpaired_chunk, size_t chunk_size) {
-	SECTION("Moving smaller elements to 'b', first move them to the beginning of 'a' using std::stable_partition");
+void fjDequeSmallerChunksToB(std::deque<int>& a, std::deque<int>& b, size_t chunk_size) {
 	std::stable_partition(a.begin(), a.end(), SmallerChunks(a, chunk_size));
-	SECTION("'a' after partition:");
-	PRINT_VAR(a);
 
 	const size_t chunks_count = a.size() / chunk_size;
 	b.resize(chunks_count / 2 * chunk_size);
@@ -114,17 +91,13 @@ void fjDequeSmallerChunksToB(std::deque<int>& a, std::deque<int>& b, std::deque<
 	std::copy(a.cbegin(), a_mid, b.begin());
 	a.erase(a.cbegin(), a_mid);
 
-	// If there's an unpaired chunk - move it to 'unpaired chunk'
+	// If there's an unpaired chunk - move it to 'b'
 	if (a.size() > b.size()) {
 		std::deque<int>::const_iterator unpaired_it = a.end();
 		std::advance(unpaired_it, -chunk_size);
-		unpaired_chunk.insert(unpaired_chunk.begin(), unpaired_it, a.cend());
+		b.insert(b.end(), unpaired_it, a.cend());
 		a.resize(a.size() - chunk_size);
 	}
-	SECTION("After 'b' is created:");
-	PRINT_VAR(a);
-	PRINT_VAR(b);
-	PRINT_VAR(unpaired_chunk);
 }
 
 void fjDequeFirstBToTheLeft(std::deque<int>& a, const std::deque<int>& b, const size_t chunk_size) {
@@ -133,128 +106,111 @@ void fjDequeFirstBToTheLeft(std::deque<int>& a, const std::deque<int>& b, const 
 	a.insert(a.cbegin(), b.cbegin(), b_first_chunk_end);
 }
 
-void fjDequeInsertion(	std::deque<int>& a,
-						const std::deque<int>::const_iterator& b_left,
-						const size_t max_index,
+void fjDequeInsertTwo(	std::deque<int>& a,
+						std::deque<int>::const_iterator& a_l_it,
+						const std::deque<int>::const_iterator& b_l_it,
 						const size_t chunk_size)
 {
-	size_t a_index = max_index / chunk_size / 2 * chunk_size;
-	std::deque<int>::const_iterator b_right = b_left;
-	std::advance(b_right, chunk_size);
-
-	try
-	{
-		PRINT_VAR(max_index);
-		if (max_index < a.size()) {
-			PRINT_VAR(a.at(max_index));
-		}
-		PRINT_VAR(a_index);
-		PRINT_VAR(a.at(a_index));
-		PRINT_VAR(*b_left);
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr	<< e.what()
-					<< "\nChunk size: " << chunk_size
-					<< "\nSize: " << a.size()
-					<< "\nmax_index: " << max_index
-					<< "\na_index: " << a_index
-					<< std::endl;
-	}
-
-
-	std::deque<int>::const_iterator a_left = a.cbegin();
+	std::deque<int>::const_iterator b_r_it = b_l_it;
+	std::advance(b_r_it, chunk_size);
 	++comparisons;
-	try
-	{
-		if (*b_left < a.at(a_index)) {
-			while (a_index >= chunk_size) {
-				++comparisons;
-				if (*b_left >= a.at(a_index - chunk_size))
-					break;
-				else
-					a_index -= chunk_size;
-			}
-		}
-		else {
-			while (a_index < max_index) {
-				a_index += chunk_size;
-				++comparisons;
-				if (*b_left <= a.at(a_index))
-					break;
-			}
-		}
+	while (a_l_it != a.cend() && *b_l_it > *a_l_it) {
+		std::advance(a_l_it, chunk_size);
+		++comparisons;
 	}
-	catch(const std::exception& e)
-	{
-		a.insert(a.end(), b_left, b_right);
-		// findMe(a);
-		return;
-		// std::cerr << e.what() << '\n';
-		// std::cerr << a << std::endl;
-	}
-
-
-	std::advance(a_left, a_index);
-	a.insert(a_left, b_left, b_right);
+	a.insert(a_l_it, b_l_it, b_r_it);
 }
 
-void fjDequeMergeMagic(	std::deque<int>& a,
-						const std::deque<int>& b,
-						const std::deque<int>& unpaired_chunk,
-						const std::deque<size_t>& jcbsthl,
-						const size_t chunk_size) {
+void fjDequeInsertThree(std::deque<int>& a,
+						const std::deque<int>::const_iterator& a_mid_it,
+						const std::deque<int>::const_iterator& b_l_it,
+						const size_t chunk_size)
+{
+	std::deque<int>::const_iterator a_temp = a_mid_it;
+	std::deque<int>::const_iterator b_r_it = b_l_it;
+	std::advance(b_r_it, chunk_size);
+
+	if (*b_l_it > *a_mid_it) {
+		++comparisons;
+		std::advance(a_temp, chunk_size);
+		++comparisons;
+		if (a_temp == a.end() || *b_l_it < *a_temp) {
+			a.insert(a_temp, b_l_it, b_r_it);
+		}
+		else {
+			std::advance(a_temp, chunk_size);
+			a.insert(a_temp, b_l_it, b_r_it);
+		}
+	}
+	else {
+		std::advance(a_temp, -chunk_size);
+		if (*b_l_it > *a_temp) {
+			++comparisons;
+			a.insert(a_mid_it, b_l_it, b_r_it);
+		}
+		else
+			a.insert(a_temp, b_l_it, b_r_it);
+	}
+}
+
+void fjDequeInsertion(	std::deque<int>& a,
+						std::deque<int>::const_iterator a_l_it,
+						const std::deque<int>::const_iterator& a_r_it,
+						const std::deque<int>::const_iterator& b_l_it,
+						const size_t chunk_size)
+{
+	const size_t distance = std::distance(a_l_it, a_r_it);
+	const size_t a_mid_index = distance / chunk_size / 2 * chunk_size;
+	std::deque<int>::const_iterator a_mid_it = a_l_it;
+	std::advance(a_mid_it, a_mid_index);
+
+	if (distance / chunk_size <= 2)
+		fjDequeInsertTwo(a, a_l_it, b_l_it, chunk_size);
+	else if (distance / chunk_size == 3)
+		fjDequeInsertThree(a, a_mid_it, b_l_it, chunk_size);
+	else if (*b_l_it > *a_mid_it) {
+		++comparisons;
+		std::advance(a_mid_it, chunk_size);
+		fjDequeInsertion(a, a_mid_it, a_r_it, b_l_it, chunk_size);
+	}
+	else
+		fjDequeInsertion(a, a_l_it, a_mid_it, b_l_it, chunk_size);
+
+}
+
+void fjDequeMergeMagic(std::deque<int>& a, const std::deque<int>& b, const size_t chunk_size) {
 
 	fjDequeFirstBToTheLeft(a, b, chunk_size);
-	SECTION("After moving first chunk to the left of 'a'");
-	PRINT_VAR(a);
+
+	std::deque<size_t>	jcbsthl;
+	const size_t chunks_in_b = b.size() / chunk_size;
+	if (chunks_in_b > 1)
+		dequeJacubsthal(jcbsthl, chunks_in_b);
 
 	for (size_t i = 1; i < jcbsthl.size(); ++i) {
-		const size_t max_index = (jcbsthl.at(i) + i) * chunk_size;
+		std::deque<int>::const_iterator a_r = a.cbegin();
+		std::advance(a_r, (jcbsthl.at(i) + i) * chunk_size);
 
 		std::deque<int>::const_iterator b_left = b.cbegin();
 		std::advance(b_left, (jcbsthl.at(i) * chunk_size));
 
-		fjDequeInsertion(a, b_left, max_index, chunk_size);
-		PRINT_VAR(unpaired_chunk);
-		PRINT_VAR(a);
-		PRINT_VAR(b);
+		fjDequeInsertion(a, a.begin(), a_r, b_left, chunk_size);
 	}
-	// if (chunk_size == 1) {
-	// 	SECTION("After insertion");
-	// 	findMe(a);
-	// }
-	if (unpaired_chunk.size())
-		fjDequeInsertion(a, unpaired_chunk.begin(), a.size(), chunk_size);
 }
 
 void fjDequeUnwrap(std::deque<int>& a, size_t chunk_size) {
-	SECTION("\nUnwrapping iteration");
-	PRINT_VAR(chunk_size);
 
 	std::deque<int>		b;
 	std::deque<int>		tail;
-	std::deque<int>		unpaired_chunk;
-	std::deque<size_t>	jcbsthl_arr;
 
 	fjDequeMakeTail(a, tail, chunk_size);
-	fjDequeSmallerChunksToB(a, b, unpaired_chunk, chunk_size);
-
-	{
-		const size_t chunks_in_a = a.size() / chunk_size;
-		if (chunks_in_a > 1)
-			dequeJacubsthal(jcbsthl_arr, chunks_in_a);
-	}
-
-	fjDequeMergeMagic(a, b, unpaired_chunk, jcbsthl_arr, chunk_size);
+	fjDequeSmallerChunksToB(a, b, chunk_size);
+	fjDequeMergeMagic(a, b, chunk_size);
 
 	if (tail.size()) {
 		if (chunk_size > 1)
 			a.insert(a.end(), tail.cbegin(), tail.cend());
 	}
-	SECTION("After unwrapping iteration:");
-	PRINT_VAR(a);
-	PRINT_VAR(comparisons);
 }
 
 void fjDequeSort(std::deque<int>& in_arr) {
@@ -280,6 +236,7 @@ int main(int argc, char** argv) {
 	}
 
 	std::deque<int>	d;
+	std::deque<int> control_d;
 
 	{
 		std::istringstream	ss;
@@ -293,12 +250,16 @@ int main(int argc, char** argv) {
 				return (42);
 			}
 			d.push_back(static_cast<int>(temp));
+			control_d.push_back(static_cast<int>(temp));
 		}
 	}
+
+	assert(d == control_d);
+
 	std::cout << "Before: " << d << std::endl;
 	fjDequeSort(d);
 	std::cout << "After: " << d << std::endl;
-
-	// findMe(d);
-	assert(std::is_sorted(d.cbegin(), d.cend()));
+	PRINT_VAR(comparisons);
+	std::sort(control_d.begin(), control_d.end());
+	assert(d == control_d);
 }
